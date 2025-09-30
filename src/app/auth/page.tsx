df6b762aba077f1bfb0169f820535ea4e9c1e118
jocesman <jicespedes1976@gmail.com>
2025-09-30 13:37:46 -0600

    Refactor: eliminación de archivos y cambios en layout/page

--- src/app/auth/page.tsx
+++
@@ -1,9 +0,0 @@
-
-const Demo: React.FC = () => {
-
-    return (
-        <h1>HOLA MUNDO</h1>)
-    
-}
-
-export default Demo;
--- src/components/card/types.ts
+++
@@ -1,4 +0,0 @@
-export interface CardProps {
-    title: string;
-    description?: string;
-}
