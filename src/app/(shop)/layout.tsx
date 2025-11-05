0caf6437cdc6509183536d349c1c34cda6f86af0
jocesman <jicespedes1976@gmail.com>
2025-11-05 14:31:49 -0600

    REINICIO

--- src/app/(shop)/layout.tsx
+++
@@ -1,8 +0,0 @@
-
-export  default function ShopLayout ({children}: {children : React.ReactNode}) {
-    return (
-        <main className="min-h-screen bg-red-500">
-            {children}
-        </main>
-    );
-}
