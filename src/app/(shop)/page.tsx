ac0163962a8e3d74ea766a70836f2fd18ef1edb6
jocesman <jicespedes1976@gmail.com>
2025-11-06 13:28:51 -0600

    configurando el Top menú

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -4,8 +4,6 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="">
-        <h1>Hola Mundo</h1>
-        <h1 className={`${titleFont.className} font-bold`}>Hola Mundo</h1>
-        <h1 className={`${titleFont.className}`}>Hola Mundo</h1>
+        <h1 className = "p-10">Page ppal</h1>
     </div>
 )};
